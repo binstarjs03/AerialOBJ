28525bd4acb4f1d5c712973ad378036e3af37723
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-08-18 07:18:10 +0700

    NbtTypeName refactor constant value

--- src/MinecraftSharpOBJ/Nbt/NbtTypeName.cs
+++ src/MinecraftSharpOBJ/Nbt/NbtTypeName.cs
@@ -1,17 +1,17 @@
 ﻿namespace binstarjs03.MinecraftSharpOBJ.Nbt;
 
 public static class NbtTypeName {
-    public static readonly string NbtEnd = "End";
-    public static readonly string NbtByte = "Byte";
-    public static readonly string NbtShort = "Short";
-    public static readonly string NbtInt = "Int";
-    public static readonly string NbtLong = "Long";
-    public static readonly string NbtFloat = "Float";
-    public static readonly string NbtDouble = "Double";
-    public static readonly string NbtArrayByte = "ByteArray";
-    public static readonly string NbtString = "String";
-    public static readonly string NbtList = "List";
-    public static readonly string NbtCompound = "Compound";
-    public static readonly string NbtArrayInt = "IntArray";
-    public static readonly string NbtArrayLong = "LongArray";
+    public static readonly string NbtEnd = nameof(Concrete.NbtEnd);
+    public static readonly string NbtByte = nameof(Concrete.NbtByte);
+    public static readonly string NbtShort = nameof(Concrete.NbtShort);
+    public static readonly string NbtInt = nameof(Concrete.NbtInt);
+    public static readonly string NbtLong = nameof(Concrete.NbtLong);
+    public static readonly string NbtFloat = nameof(Concrete.NbtFloat);
+    public static readonly string NbtDouble = nameof(Concrete.NbtDouble);
+    public static readonly string NbtArrayByte = nameof(Concrete.NbtArrayByte);
+    public static readonly string NbtString = nameof(Concrete.NbtString);
+    public static readonly string NbtList = nameof(Concrete.NbtList);
+    public static readonly string NbtCompound = nameof(Concrete.NbtCompound);
+    public static readonly string NbtArrayInt = nameof(Concrete.NbtArrayInt);
+    public static readonly string NbtArrayLong = nameof(Concrete.NbtArrayLong);
 }
