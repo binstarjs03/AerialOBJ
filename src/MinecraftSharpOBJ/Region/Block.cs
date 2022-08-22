3bc5ec3471efc489b4c1c2d2c69fc1678a40b0aa
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-08-22 17:34:02 +0700

    Implement Region

---
+++ src/MinecraftSharpOBJ/Region/Block.cs
@@ -0,0 +1,46 @@
+﻿using binstarjs03.MinecraftSharpOBJ.Utils;
+using binstarjs03.MinecraftSharpOBJ.Nbt.Abstract;
+using binstarjs03.MinecraftSharpOBJ.Nbt.Concrete;
+namespace binstarjs03.MinecraftSharpOBJ.Region;
+
+public class Block {
+    private readonly string _name;
+    private readonly Coords3 _coordsRel;
+    private readonly Coords3 _coordsAbs;
+    private readonly NbtCompound _properties;
+
+    public Block(Section section, Coords3 coordsRel) {
+        _coordsRel = coordsRel;
+        _coordsAbs = EvaluateCoordsAbs(section, coordsRel);
+        string name = "minecraft:air";
+        NbtBase[] propertiesNbt = new NbtBase[1] { new NbtString("Name", name) };
+        _name = name;
+        _properties = new(propertiesNbt);
+    }
+
+    public Block(Section section, Coords3 coordsRel, NbtCompound properties) {
+        _coordsRel = coordsRel;
+        _coordsAbs = EvaluateCoordsAbs(section, coordsRel);
+        _name = properties.Get<NbtString>("Name").Value;
+        _properties = properties;
+    }
+
+    private static Coords3 EvaluateCoordsAbs(Section section, Coords3 coordsRel) {
+        int x = coordsRel.x + (section.CoordsAbs.x * Section.BlockCount);
+        int y = coordsRel.y + (section.CoordsAbs.y * Section.BlockCount);
+        int z = coordsRel.z + (section.CoordsAbs.z * Section.BlockCount);
+        return new Coords3(x, y, z);
+    }
+
+    public string Name => _name;
+
+    public Coords3 CoordsRel => _coordsRel;
+
+    public Coords3 CoordsAbs => _coordsAbs;
+
+    public NbtCompound Properties => _properties;
+
+    public override string ToString() {
+        return $"{_name} at {_coordsAbs}";
+    }
+}
---
+++ src/MinecraftSharpOBJ/Region/Chunk.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using binstarjs03.MinecraftSharpOBJ.Utils;
+using binstarjs03.MinecraftSharpOBJ.Nbt.Concrete;
+namespace binstarjs03.MinecraftSharpOBJ.Region;
+
+public class Chunk {
+    public static readonly int TotalSectionCount = SectionRange.max - SectionRange.min;
+    public static readonly (int min, int max) SectionRange = (-4, 19);
+    public static readonly int TotalBlockCount = Section.TotalBlockCount * TotalSectionCount;
+    public static readonly Coords3Range BlockRangeRel = new(
+        min: new Coords3(
+            x: 0,
+            y: Section.BlockCount * SectionRange.min,
+            z: 0),
+        max: new Coords3(
+            x: Section.BlockCount,
+            y: Section.BlockCount * SectionRange.max + Section.BlockRange,
+            z: Section.BlockCount)
+    );
+
+    private readonly Coords2 _coordsRel;
+    private readonly Coords2 _coordsAbs;
+    private readonly Coords3Range _blockRangeAbs;
+    private readonly NbtCompound _nbtChunk;
+    private readonly Dictionary<int, NbtCompound> _nbtSections;
+
+    public Chunk(NbtCompound nbtChunk, Coords2 coordsRel) {
+        _coordsRel = coordsRel;
+        _coordsAbs = evaluateCoordsAbs(nbtChunk);
+        _blockRangeAbs = evaluateBlockRangeAbs(_coordsAbs);
+        _nbtChunk = nbtChunk;
+        _nbtSections = initNbtSections(nbtChunk);
+
+        static Coords2 evaluateCoordsAbs(NbtCompound nbtChunk) {
+            int x = nbtChunk.Get<NbtInt>("xPos").Value;
+            int z = nbtChunk.Get<NbtInt>("zPos").Value;
+            return new Coords2(x, z);
+        }
+        static Coords3Range evaluateBlockRangeAbs(Coords2 coordsAbs) {
+            int minAbsBx = coordsAbs.x * Section.BlockCount;
+            int minAbsBy = SectionRange.min * Section.BlockCount;
+            int minAbsBz = coordsAbs.z * Section.BlockCount;
+            Coords3 minAbsB = new(minAbsBx, minAbsBy, minAbsBz);
+
+            int maxAbsBx = minAbsBx + Section.BlockRange;
+            int maxAbsBy = SectionRange.max * Section.BlockCount + Section.BlockRange;
+            int maxAbsBz = minAbsBz + Section.BlockRange;
+            Coords3 maxAbsB = new(maxAbsBx, maxAbsBy, maxAbsBz);
+
+            return new Coords3Range(minAbsB, maxAbsB);
+        }
+        static Dictionary<int, NbtCompound> initNbtSections(NbtCompound nbtChunk) {
+            Dictionary<int, NbtCompound> ret = new();
+            NbtList nbtSections = nbtChunk.Get<NbtList>("sections");
+            foreach (NbtCompound nbtSection in nbtSections.Tags.Cast<NbtCompound>()) {
+                ret.Add(nbtSection.Get<NbtByte>("Y").Value, nbtSection);
+            }
+            return ret;
+        }
+    }
+
+    public Coords2 CoordsRel => _coordsRel;
+
+    public Coords2 CoordsAbs => _coordsAbs;
+
+    public Coords3Range BlockRangeAbs => _blockRangeAbs;
+
+    public NbtCompound NbtChunk => _nbtChunk;
+
+    public Section GetSection(int yPos) {
+        if (!_nbtSections.ContainsKey(yPos))
+            throw new KeyNotFoundException($"Section {yPos} does not exist in chunk");
+        return new Section(this, _nbtSections[yPos]);
+    }
+
+    public Block GetBlock(Coords3 coords, bool relative) {
+        int sectionYPos = (int)MathF.Floor(coords.y / Section.BlockCount);
+        if (relative)
+            coords.y = MathUtils.Mod(coords.y, Section.BlockCount);
+        //    BlockRangeRel.IsOutside(coords);
+        //else {
+        //    BlockRangeAbs.IsOutside(coords);
+        //    coords = ConvertBlockAbsToRel(coords);
+        //}
+        Section section = GetSection(sectionYPos);
+        return section.GetBlock(coords, relative);
+    }
+
+    public static Coords3 ConvertBlockAbsToRel(Coords3 coords) {
+        int relBx = MathUtils.Mod(coords.x, Section.BlockCount);
+        int relBy = coords.y;
+        int relBz = MathUtils.Mod(coords.z, Section.BlockCount);
+        return new Coords3(relBx, relBy, relBz);
+    }
+}
