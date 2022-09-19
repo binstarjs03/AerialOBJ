9f206928a553569d1ae0ba0177f0bf51e6fe5de9
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-09-19 14:26:43 +0700

    wpf converter BoolToVisibility
    implement Collapse / Hide variant on boolean conversion

--- src/MineSharpOBJ/WpfApp/BindingConverters/BoolToVisibilityCollapse.cs
+++ src/MineSharpOBJ/WpfApp/BindingConverters/BoolToVisibilityCollapse.cs
@@ -4,21 +4,21 @@ using System.Windows.Data;
 using System.Windows;
 namespace binstarjs03.MineSharpOBJ.WpfApp.BindingConverters;
 
-public class BoolToVisibility : IValueConverter {
+public class BoolToVisibilityCollapse : IValueConverter {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
         bool isTrue = (bool)value;
         if (isTrue)
             return Visibility.Visible;
         else
-            return Visibility.Hidden;
+            return Visibility.Collapsed;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
         Visibility visibility = (Visibility)value;
         return visibility switch {
             Visibility.Visible => true,
-            Visibility.Hidden => false,
-            _ => false,
+            Visibility.Collapsed => false,
+            _ => throw new Exception($"{nameof(BoolToVisibilityCollapse)} convert back not returning visible or collapsed"),
         };
     }
 }
