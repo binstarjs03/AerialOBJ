1798d3023ed77aba18b0b52e7e5ec14f9e2145d6
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-09-24 22:00:05 +0700

    wpf Global exception handling

--- src/MineSharpOBJ/WpfApp/App.xaml.cs
+++ src/MineSharpOBJ/WpfApp/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Threading;
+
 namespace binstarjs03.MineSharpOBJ.WpfApp;
 
 public partial class App : Application {
@@ -8,4 +10,18 @@ public partial class App : Application {
 	}
 
     public static DateTime LauchTime { get; set; }
+
+	private void OnStartup(object sender, StartupEventArgs e)
+	{
+		Current.DispatcherUnhandledException += OnUnhandledException;
+	}
+
+	private void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+	{
+		string msg = $"An unhandled exception occured: \n"
+				   + $"{e.Exception}\n"
+				   + $"{e.Exception.Message}\n\n";
+
+        MessageBox.Show(msg, "MineSharpOBJ Crashed", MessageBoxButton.OK, MessageBoxImage.Error);
+	}
 }
