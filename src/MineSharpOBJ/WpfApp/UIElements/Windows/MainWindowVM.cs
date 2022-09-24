5669739b974fc7eadc0826779f50a2321f0d1cbc
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-09-24 22:05:13 +0700

    implement Open & Close session

--- src/MineSharpOBJ/WpfApp/UIElements/Windows/MainWindowVM.cs
+++ src/MineSharpOBJ/WpfApp/UIElements/Windows/MainWindowVM.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 using binstarjs03.MineSharpOBJ.WpfApp.Services;
@@ -18,10 +19,19 @@ public class MainWindowVM : ViewModelWindow<MainWindowVM, MainWindow>
         SharedProperty.PropertyChanged += OnSharedPropertyChanged;
 
         // assign command implementation to commands
+        OpenCommand = new RelayCommand(OnOpen);
+        CloseCommand = new RelayCommand(OnClose);
         AboutCommand = new RelayCommand(OnAbout);
     }
 
     // States -----------------------------------------------------------------
+
+    private string _title = "MineSharpOBJ";
+    public string Title
+    {
+        get => _title;
+        set => SetAndNotifyPropertyChanged(value, ref _title);
+    }
 
     public bool IsDebugLogWindowVisible
     {
@@ -39,6 +49,37 @@ public class MainWindowVM : ViewModelWindow<MainWindowVM, MainWindow>
     private void OnAbout(object? arg)
     {
         ModalService.ShowAboutModal();
+    }
+
+    public ICommand OpenCommand { get; }
+    private void OnOpen(object? arg)
+    {
+        LogService.Log("Attempting to loading savegame...");
+        using FolderBrowserDialog dialog = new();
+        DialogResult dialogResult = dialog.ShowDialog();
+        if (dialogResult != DialogResult.OK)
+        {
+            LogService.Log("Dialog cancelled. Aborting...", useSeparator: true);
+            return;
+        }
+        SessionInfo? session = IOService.LoadSavegame(dialog.SelectedPath);
+        if (session is null)
+        {
+            LogService.Log("Failed changing session. Aborting...", useSeparator: true);
+            return;
+        }
+        SharedProperty.SessionInfo = session;
+        Title = $"MineSharpOBJ - {session.WorldName}";
+        LogService.Log("Successfully changed session.", useSeparator: true);
+    }
+
+    public ICommand CloseCommand { get; }
+    private void OnClose(object? arg)
+    {
+        LogService.Log("Attempting to closing savegame...");
+        Title = "MineSharpOBJ";
+        SharedProperty.SessionInfo = null;
+        LogService.Log("Successfully closed session.", useSeparator: true);
     }
 
     // Event Handlers ---------------------------------------------------------
