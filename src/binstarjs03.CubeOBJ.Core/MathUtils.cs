2fa7651323d71c5ce9a156637c349caad6de5b79
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-09 06:48:44 +0700

    Core implement BinaryUtils, MathUtils & NonNullString

---
+++ src/binstarjs03.CubeOBJ.Core/MathUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace binstarjs03.CubeOBJ.Core;
+
+public static class MathUtils
+{
+    public static int Mod(int left, int right)
+    {
+        int result = left % right;
+        if (right >= 0 && result < 0 || right < 0 && result >= 0)
+            result += right;
+        return result;
+    }
+}
