a9c8c55c1bc32133a1856291cb9a19dc30e5944d
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-09 12:23:10 +0700

    wpf implement IntBox, DoubleBox & XYZIntBox

---
+++ src/binstarjs03.CubeOBJ.WpfApp/UIElements/Components/DoubleBox.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace binstarjs03.CubeOBJ.WpfApp.UIElements.Components;
+
+public partial class DoubleBox : TextBox
+{
+    public DoubleBox()
+    {
+        InitializeComponent();
+    } 
+
+    public double DoubleValue
+    {
+        get { return (double)GetValue(DoubleValueProperty); }
+        set { SetValue(DoubleValueProperty, value); }
+    }
+
+    public static readonly DependencyProperty DoubleValueProperty =
+        DependencyProperty.Register(nameof(DoubleValue), typeof(double), typeof(DoubleBox), new PropertyMetadata(0.0));
+
+    private void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+            UpdateValueBinding();
+    }
+
+    private void OnLostFocus(object sender, RoutedEventArgs e)
+    {
+        UpdateValueBinding();
+    }
+
+    private void UpdateValueBinding()
+    {
+        BindingExpression expr = Root.GetBindingExpression(TextProperty);
+        expr.UpdateSource();
+    }
+}
---
+++ src/binstarjs03.CubeOBJ.WpfApp/UIElements/Components/XYZIntBox.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace binstarjs03.CubeOBJ.WpfApp.UIElements.Components;
+
+public partial class XYZIntBox : UserControl
+{
+    public XYZIntBox()
+    {
+        InitializeComponent();
+    }
+
+    public string Header
+    {
+        get { return (string)GetValue(HeaderProperty); }
+        set { SetValue(HeaderProperty, value); }
+    }
+    public int XValue
+    {
+        get { return (int)GetValue(XValueProperty); }
+        set { SetValue(XValueProperty, value); }
+    }
+    public int YValue
+    {
+        get { return (int)GetValue(YValueProperty); }
+        set { SetValue(YValueProperty, value); }
+    }
+    public int ZValue
+    {
+        get { return (int)GetValue(ZValueProperty); }
+        set { SetValue(ZValueProperty, value); }
+    }
+
+    public static readonly DependencyProperty HeaderProperty =
+        DependencyProperty.Register(nameof(Header), typeof(string), typeof(XYZIntBox), new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty XValueProperty =
+        DependencyProperty.Register(nameof(XValue), typeof(int), typeof(XYZIntBox), new PropertyMetadata(0));
+
+    public static readonly DependencyProperty YValueProperty =
+        DependencyProperty.Register(nameof(YValue), typeof(int), typeof(XYZIntBox), new PropertyMetadata(0));
+
+    public static readonly DependencyProperty ZValueProperty =
+        DependencyProperty.Register(nameof(ZValue), typeof(int), typeof(XYZIntBox), new PropertyMetadata(0));
+}
