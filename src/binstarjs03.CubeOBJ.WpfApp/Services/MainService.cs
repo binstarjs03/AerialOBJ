9b8b12589043b0c6872b3a44366dd81b94d6ac82
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-10 09:04:37 +0700

    wpf implement services

---
+++ src/binstarjs03.CubeOBJ.WpfApp/Services/MainService.cs
@@ -0,0 +1,12 @@
+﻿namespace binstarjs03.CubeOBJ.WpfApp.Services;
+
+public static class MainService
+{
+    public static void Initialize()
+    {
+        LogService.LogRuntimeInfo();
+        LogService.Log("Starting Initialization...");
+        LogService.Log("Initialization complete", useSeparator: true);
+    }
+}
+
