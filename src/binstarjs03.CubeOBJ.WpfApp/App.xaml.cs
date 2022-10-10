cef19cfa7b0f60149df74ddd959fd0ae9975bdd5
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-10 09:05:34 +0700

    wpf implement global exception handling

--- src/binstarjs03.CubeOBJ.WpfApp/App.xaml.cs
+++ src/binstarjs03.CubeOBJ.WpfApp/App.xaml.cs
@@ -1,5 +1,42 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+using binstarjs03.CubeOBJ.WpfApp.Services;
 
 namespace binstarjs03.CubeOBJ.WpfApp;
 
-public partial class App : Application { }
+public partial class App : Application
+{
+    public App()
+    {
+        LauchTime = DateTime.Now;
+    }
+
+    public static DateTime LauchTime { get; set; }
+
+    private void OnStartup(object sender, StartupEventArgs e)
+    {
+        Current.DispatcherUnhandledException += OnUnhandledException;
+    }
+
+    private void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        string msg = $"An unhandled exception occured: \n"
+                   + $"{e.Exception}\n"
+                   + $"{e.Exception.Message}\n\n";
+        LogService.LogError("Critical Error!");
+        LogService.Log(msg);
+        MessageBox.Show(msg, "MineSharpOBJ Crashed", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        string lauchTime = LauchTime.ToString().Replace('/', '-').Replace(':', '-');
+        string path = $"{Environment.CurrentDirectory}/MineSharpOBJ Crash Log {lauchTime}.txt";
+
+        string logSaveMsg;
+        if (LogService.WriteLogToDiskOnCrashed(path))
+            logSaveMsg = $"Debug log content has been saved to {path}";
+        else
+            logSaveMsg = $"Failed writing Debug log content to {path}";
+        MessageBox.Show(logSaveMsg, "MineSharpOBJ Crashed", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+}
