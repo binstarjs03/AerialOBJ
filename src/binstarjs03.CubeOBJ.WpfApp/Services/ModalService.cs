34e56be98a33002dab0edb3cd508e0ac13ada096
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-10 09:06:23 +0700

    wpf implement modal

---
+++ src/binstarjs03.CubeOBJ.WpfApp/Services/ModalService.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+using binstarjs03.CubeOBJ.WpfApp.UIElements.Modals;
+
+namespace binstarjs03.CubeOBJ.WpfApp.Services;
+
+public static class ModalService
+{
+    public static void ShowAboutModal()
+    {
+        new AboutModal().ShowDialog();
+    }
+
+    public static void ShowErrorOK(string caption, string errorMsg)
+    {
+        MessageBox.Show(errorMsg,
+                        caption,
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+    }
+}
+
---
+++ src/binstarjs03.CubeOBJ.WpfApp/UIElements/Modals/AboutModal.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace binstarjs03.CubeOBJ.WpfApp.UIElements.Modals;
+
+public partial class AboutModal : Window
+{
+    public AboutModal()
+    {
+        InitializeComponent();
+    }
+
+    private void OnClickClose(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
