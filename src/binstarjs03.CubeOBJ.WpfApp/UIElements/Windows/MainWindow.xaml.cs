f1a064b13725af819a2fe05896740dba6296177f
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-10 11:18:30 +0700

    wpf DebugLogWindow synchronize position

--- src/binstarjs03.CubeOBJ.WpfApp/UIElements/Windows/MainWindow.xaml.cs
+++ src/binstarjs03.CubeOBJ.WpfApp/UIElements/Windows/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 
+using binstarjs03.CubeOBJ.WpfApp.Services;
+
 namespace binstarjs03.CubeOBJ.WpfApp.UIElements.Windows;
 
 public partial class MainWindow : Window
@@ -7,5 +9,34 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowVM(this);
+        
+        DebugLogWindow = new DebugLogWindow();
+        Show();
+        DebugLogWindow.Owner = this;
+    }
+
+    public DebugLogWindow DebugLogWindow { get; }
+
+    private void SynchronizeDebugLogWindowPosition()
+    {
+        DebugLogWindow.Top = Top;
+        DebugLogWindow.Left = Left + ActualWidth;
+    }
+
+    private void OnLocationChanged(object sender, System.EventArgs e)
+    {
+        SynchronizeDebugLogWindowPosition();
+    }
+
+    private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        SynchronizeDebugLogWindowPosition();
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        MainService.Initialize();
+
     }
 }
