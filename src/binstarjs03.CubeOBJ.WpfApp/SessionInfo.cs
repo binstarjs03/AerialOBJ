27ab2274a15271f0f8dc6d55dc1ff36b1082141f
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-10 09:07:23 +0700

    wpf implement sessioninfo

---
+++ src/binstarjs03.CubeOBJ.WpfApp/SessionInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+using binstarjs03.CubeOBJ.Core.Nbt;
+
+namespace binstarjs03.CubeOBJ.WpfApp;
+
+public class SessionInfo
+{
+    public SessionInfo(DirectoryInfo savegameDirectory, NbtCompound nbtLevel)
+    {
+        WorldName = nbtLevel.Get<NbtCompound>("Data")
+                            .Get<NbtString>("LevelName")
+                            .Value;
+        SavegameDirectory = savegameDirectory;
+    }
+
+    public string WorldName { get; }
+
+    public DirectoryInfo SavegameDirectory { get; }
+}
