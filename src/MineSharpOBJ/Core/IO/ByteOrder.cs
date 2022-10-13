8b10d6951f24295969fbe31a5ea10098c780b9a6
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-13 09:14:40 +0700

    Prepare Rewrite whole thing

--- src/MineSharpOBJ/Core/IO/ByteOrder.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace binstarjs03.MineSharpOBJ.Core.Utils.IO;
-
-public enum ByteOrder {
-    BigEndian = 0,
-    LittleEndian = 1,
-}
--- src/MineSharpOBJ/WpfApp/Services/MainService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace binstarjs03.MineSharpOBJ.WpfApp.Services;
-
-public static class MainService {
-    public static void Initialize() {
-        LogService.LogRuntimeInfo();
-        LogService.Log("Starting Initialization...");
-        LogService.Log("Initialization complete", useSeparator: true);
-    }  
-}
