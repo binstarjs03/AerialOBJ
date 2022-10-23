7e67a4c4b2aefa7c2ebb9c329a16bb34c750eef2
binstarjs03 <93080026+binstarjs03@users.noreply.github.com>
2022-10-23 13:28:59 +0700

    implement ListUtils FisherYatesShuffle

---
+++ src/binstarjs03.AerialOBJ.Core/ListUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace binstarjs03.AerialOBJ.Core;
+public static class ListUtils
+{
+    private static Random s_random = new();
+
+    public static void FisherYatesShuffe<T>(this IList<T> list)
+    {
+        int index = list.Count;
+        while (index > 0)
+        {
+            index--;
+            int randomIndex = s_random.Next(index + 1);
+            T temp = list[randomIndex];
+            list[randomIndex] = list[index];
+            list[index] = temp;
+        }
+    }
+}
