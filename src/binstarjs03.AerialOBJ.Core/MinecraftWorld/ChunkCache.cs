37b2973c1e36b19620f68b24c31bc7be177c589a
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-11-15 18:12:22 +0700

    Improved ChunkCache
    ChunkCache no longer use sorted dictionary, which sorts it internally, which will slow execution time

--- src/binstarjs03.AerialOBJ.Core/MinecraftWorld/ChunkCache.cs
+++ src/binstarjs03.AerialOBJ.Core/MinecraftWorld/ChunkCache.cs
@@ -27,20 +27,17 @@ using System.Linq;
 using binstarjs03.AerialOBJ.Core.CoordinateSystem;
 
 namespace binstarjs03.AerialOBJ.Core.MinecraftWorld;
+
 public class ChunkCache
 {
-    private readonly SortedDictionary<int, string>[,] _highestBlocks
-        = new SortedDictionary<int, string>[Section.BlockCount, Section.BlockCount];
+    private readonly List<int>[,] _highestBlockHeights = new List<int>[Section.BlockCount, Section.BlockCount];
+    private readonly List<string>[,] _highestBlockNames = new List<string>[Section.BlockCount, Section.BlockCount];
 
     public ChunkCache(Chunk chunk)
     {
-        // this is where we build our actual cache of highest block
         for (int x = 0; x < Section.BlockCount; x++)
             for (int z = 0; z < Section.BlockCount; z++)
             {
-                SortedDictionary<int, string> highestBlockEntry = new();
-                _highestBlocks[x, z] = highestBlockEntry;
-
                 /* Concept:
                  * check if last height level block is different than current,
                  * if so, then the heighest block starts differently.
@@ -56,68 +53,85 @@ public class ChunkCache
                  * - Entry 2: 15 -> dirt
                 */
 
-                // iterate through all sections then iterate through
-                // all sections height levels (which has 16 height levels)
+                List<int> highestBlockHeights = new();
+                List<string> highestBlockNames = new();
+
 
                 // initialize last block to whatever block is at lowest section at lowest Y level
                 Section lowestSection = chunk.GetSectionAt(0);
                 Block lowestBlock = lowestSection.GetBlockPalette(new Coords3(x, 0, z));
-                int highestHeight = chunk.GetSectionAt(chunk.SectionsYPos.Length - 1).SectionCoordsAbs.Y * Section.BlockCount + Section.BlockCount - 1;
                 string lastBlockName = lowestBlock.Name;
+                
+                int highestHeight = chunk.GetSectionAt(chunk.SectionsYPos.Length - 1).SectionCoordsAbs.Y * Section.BlockCount + Section.BlockCount - 1;
 
+                // iterate through all sections from bottom to top
                 for (int index = 0; index < chunk.SectionsYPos.Length; index++)
                 {
-                    int sectionPosition = chunk.SectionsYPos[index];
-                    Section section = chunk.GetSection(sectionPosition);
+                    int sectionPos = chunk.SectionsYPos[index];
+                    Section section = chunk.GetSection(sectionPos);
+
+                    // iterate through all section height levels from bottom to top
+                    // (which has 16 height levels)
                     for (int y = 0; y < Section.BlockCount; y++)
                     {
                         Coords3 blockCoordsRel = new(x, y, z);
                         Block block = section.GetBlockPalette(blockCoordsRel);
 
                         int currentHeightLevel = section.SectionCoordsAbs.Y * Section.BlockCount + y;
-                        int lastHeightLevel = currentHeightLevel - 1;
+
 
                         // if we don't do this statement, the highest-most block will not be added
                         // as mostly above it is an air. if it was plains biome, most likely the last
                         // block is grass
                         if (currentHeightLevel == highestHeight)
                         {
-                            highestBlockEntry.Add(highestHeight, lastBlockName);
+                            highestBlockHeights.Add(highestHeight);
+                            highestBlockNames.Add(lastBlockName);
                             break;
                         }
                         else if (block.Name == lastBlockName || Block.IsAir(block))
                             continue;
 
-                        highestBlockEntry.Add(lastHeightLevel, lastBlockName);
+                        int lastHeightLevel = currentHeightLevel - 1;
+                        // block is different, make new cache entry
+                        highestBlockHeights.Add(lastHeightLevel);
+                        highestBlockNames.Add(lastBlockName);
                         lastBlockName = block.Name;
                     }
                 }
+                _highestBlockHeights[x, z] = highestBlockHeights;
+                _highestBlockNames[x, z] = highestBlockNames;
             }
     }
 
-    public void GetHighestBlock(string[,] buffer, int? heightLimit = null)
+    public void GetHighestBlock(ChunkHighestBlockInfo highestBlock, int? heightLimit = null)
     {
         int limit = (int)(heightLimit is null ? int.MaxValue : heightLimit);
         for (int x = 0; x < Section.BlockCount; x++)
             for (int z = 0; z < Section.BlockCount; z++)
             {
-                SortedDictionary<int, string> highestBlockEntry = _highestBlocks[x, z];
+                List<int> highestBlockHeights = _highestBlockHeights[x,z];
+                List<string> highestBlockNames = _highestBlockNames[x,z];
 
-                // iterate through all highest block heights until it is
-                // higher than limit (or exhausted), then select the block
-                // at that height as the highest block
+                // iterate through all index, get the height level at that index,
+                // and compare if it is higher than limit (or exhausted).
+                // If so, select the block at that index as the highest block
 
-                // initialize lastHeight to lowest block
-                int lastHeight = highestBlockEntry.Keys.First();
-                foreach (int height in highestBlockEntry.Keys)
+                // TODO start from middle, and then determine either goes up or down
+                // depends on if middle is lower or above than limit
+                // 
+                // find which index is the highest block
+                int index = 0;
+                for (; index < highestBlockHeights.Count - 1; index++)
                 {
-                    lastHeight = height;
-                    if (height > limit)
+                    int height = highestBlockHeights[index];
+                    if (height >= limit)
                         break;
                 }
 
-                // set the highest block at height limit to whatever block is at lastHeight
-                buffer[x, z] = highestBlockEntry[lastHeight];
+                // set the highest block at height limit to whatever block is at index
+                highestBlock.Names[x, z] = highestBlockNames[index];
+                highestBlock.Heights[x, z] = highestBlockHeights[index];
             }
     }
 }
