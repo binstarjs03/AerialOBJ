94ec33e067f6635a9e0c2ddeadb41f4655811abe
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-11-16 17:45:42 +0700

    Concurrency read nbt test

--- src/binstarjs03.AerialOBJ.ConsoleApp/Program.cs
+++ src/binstarjs03.AerialOBJ.ConsoleApp/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace binstarjs03.AerialOBJ.ConsoleApp;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using binstarjs03.AerialOBJ.Core;
+using binstarjs03.AerialOBJ.Core.CoordinateSystem;
+using binstarjs03.AerialOBJ.Core.MinecraftWorld;
+using binstarjs03.AerialOBJ.Core.Nbt;
+
+namespace binstarjs03.AerialOBJ.ConsoleApp;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        
+        string path = @"C:\Users\Bin\AppData\Roaming\.minecraft\saves\Terralith 2\region\r.0.0.mca";
+        Region region = Region.Open(path);
+        List<Coords2> generatedChunks = new(region.GetGeneratedChunksAsCoordsRel().generatedChunksList);
+        List<INbt> nbts = new(generatedChunks.Count);
+
+        Task[] tasks = new Task[Environment.ProcessorCount];
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            int id = i;
+            tasks[i] = new Task(() => ReadChunk(region, generatedChunks, nbts, id, tasks.Length), TaskCreationOptions.LongRunning);
+        }
+        for (int i = 0; i < tasks.Length; i++)
+            tasks[i].Start();
+        Task.WaitAll(tasks);
+        Console.WriteLine();
+    }
+
+    private static void ReadChunk(Region region, List<Coords2> list, List<INbt> nbts, int id, int taskCount)
+    {
+        int partition = list.Count / taskCount;
+        int min = partition * id;
+        int max = min + partition;
+        for (int i = min; i < max; i++)
+        {
+            Coords2 chunkCoords = list[i];
+            nbts.Add(region.GetChunkNbt(chunkCoords, true));
+        }
     }
 }
