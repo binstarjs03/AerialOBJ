14d6660aab445d3dde77dd15a38ff4ed8caa93e2
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-11-21 17:03:30 +0700

    ViewportView add data context from code

--- src/binstarjs03.AerialOBJ.WpfAppNew/View/ViewportControl.xaml.cs
+++ src/binstarjs03.AerialOBJ.WpfAppNew/View/ViewportControl.xaml.cs
@@ -1,20 +1,45 @@
-﻿using System.Windows;
+﻿using System.Drawing;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 using binstarjs03.AerialOBJ.WpfAppNew.Components.Interfaces;
+using binstarjs03.AerialOBJ.WpfAppNew.ViewModel;
 
 namespace binstarjs03.AerialOBJ.WpfAppNew.View;
 
-public partial class ViewportControl : UserControl
+public partial class ViewportControl : UserControl, IViewportView
 {
     public ViewportControl()
     {
         InitializeComponent();
-        (DataContext as IViewport)!.RequestFocusToViewport += OnFocusToViewportRequested;
+        DataContext = new ViewportControlVM(this);
     }
 
-    private void OnFocusToViewportRequested()
+    public new void Focus()
     {
         ViewportPanel.Focus();
+        Keyboard.Focus(ViewportPanel);
+    }
+
+    public void AddToCanvas(UIElement uiElement)
+    {
+        ViewportCanvas.Children.Add(uiElement);
+    }
+
+    public bool HasCanvasItem(UIElement uiElement)
+    {
+        return ViewportCanvas.Children.Contains(uiElement);
+    }
+
+    public void RemoveFromCanvas(UIElement uiElement)
+    {
+        ViewportCanvas.Children.Remove(uiElement);
+    }
+
+    public void UpdateCanvasItemPosition(UIElement uiElement, System.Windows.Point newPosition)
+    {
+        Canvas.SetLeft(uiElement, newPosition.X);
+        Canvas.SetTop(uiElement, newPosition.Y);
     }
 }
