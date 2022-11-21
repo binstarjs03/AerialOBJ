defe81696c476fc5e49f230b9388ffff36aee808
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-11-21 16:56:04 +0700

    Implement self-contained abstract Viewport

---
+++ src/binstarjs03.AerialOBJ.WpfAppNew/Components/ChunkRegionViewport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using binstarjs03.AerialOBJ.WpfAppNew.Models;
+
+namespace binstarjs03.AerialOBJ.WpfAppNew.Components;
+public class ChunkRegionViewport : Viewport
+{
+    public event Action? HeightLimitChanged;
+
+    private int _heightLimit = 319;
+    private List<RegionModel> _regionModels = new();
+
+    public int HeightLimit
+    {
+        get => _heightLimit;
+        set
+        {
+            if (value != _heightLimit)
+            {
+                _heightLimit = value;
+                HeightLimitChanged?.Invoke();
+            }
+        }
+    }
+
+    public ChunkRegionViewport() : base() { }
+
+    protected override void Update()
+    {
+        //throw new NotImplementedException();
+    }
+}
