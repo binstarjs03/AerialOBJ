ab04eff21cc26357da88c2d0c17d8b99dd195936
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-11-21 17:07:34 +0700

    Decouple viewport code from ViewportControlVM
    Decoupling is done for making it more unit testable

--- src/binstarjs03.AerialOBJ.WpfAppNew/ViewModel/ViewportControlVM.cs
+++ src/binstarjs03.AerialOBJ.WpfAppNew/ViewModel/ViewportControlVM.cs
@@ -13,24 +13,23 @@ using CommunityToolkit.Mvvm.Input;
 namespace binstarjs03.AerialOBJ.WpfAppNew.ViewModel;
 
 #pragma warning disable CA1822 // Mark members as static
-public partial class ViewportControlVM : BaseViewModel, IViewport
+public partial class ViewportControlVM : BaseViewModel
 {
     private const double s_zoomRatio = 1.5;
-
-    [ObservableProperty] private Vector _cameraPos = new(0, 0);
-    [ObservableProperty] private double _zoomLevel = 1;
-    [ObservableProperty] private int _heightLimit = 319;
-    [ObservableProperty] private Size _screenSize = new(0, 0);
+    private readonly IViewportView _viewportView;
+    private readonly ChunkRegionViewport _viewport = new();
 
     [ObservableProperty] private Point _mousePos = new(0, 0);
-    [ObservableProperty] private Point _mousePosDelta = new(0, 0);
+    [ObservableProperty] private Vector _mousePosDelta = new(0, 0);
     [ObservableProperty] private bool _mouseClickHolding = false;
     [ObservableProperty] private bool _mouseInitClickDrag = true;
     [ObservableProperty] private bool _mouseIsOutside = true;
 
-    public event Action? RequestFocusToViewport;
-
-    public double UnitScale => _zoomLevel;
+    public Point CameraPos => _viewport.CameraPos;
+    public double ZoomLevel => _viewport.ZoomLevel;
+    public Size ScreenSize => _viewport.ScreenSize;
+    public int HeightLimit => _viewport.HeightLimit;
+    public double UnitScale => _viewport.ZoomLevel;
     public double PixelPerBlock => UnitScale;
     public double PixelPerChunk => PixelPerBlock * Section.BlockCount;
     public double PixelPerRegion => PixelPerChunk * Region.ChunkCount;
@@ -47,11 +46,28 @@ public partial class ViewportControlVM : BaseViewModel, IViewport
     }
 
 
-    public ViewportControlVM()
+    public ViewportControlVM(IViewportView viewportView)
     {
         SharedStateService.ViewportSidePanelVisibilityChanged += OnSidePanelVisibilityChanged;
         SharedStateService.ViewportSidePanelDebugInfoVisibilityChanged += OnSidePanelDebugInfoVisibilityChanged;
+        _viewport.CameraPosChanged += OnViewportCameraPosChanged;
+        _viewport.ZoomLevelChanged += OnViewportZoomLevelChanged;
+        _viewport.ScreenSizeChanged += OnViewportScreenSizeChanged;
+        _viewport.HeightLimitChanged += OnViewportHeightLimitChanged;
+        _viewportView = viewportView;
     }
+
+    private void OnViewportCameraPosChanged() => OnPropertyChanged(nameof(CameraPos));
+    private void OnViewportZoomLevelChanged()
+    {
+        OnPropertyChanged(nameof(ZoomLevel));
+        OnPropertyChanged(nameof(UnitScale));
+        OnPropertyChanged(nameof(PixelPerBlock));
+        OnPropertyChanged(nameof(PixelPerChunk));
+        OnPropertyChanged(nameof(PixelPerRegion));
+    }
+    private void OnViewportScreenSizeChanged() => OnPropertyChanged(nameof(ScreenSize));
+    private void OnViewportHeightLimitChanged() => OnPropertyChanged(nameof(HeightLimit));
 
     private void OnSidePanelVisibilityChanged(bool obj) => OnPropertyChanged(nameof(IsSidePanelVisible));
     private void OnSidePanelDebugInfoVisibilityChanged(bool obj) => OnPropertyChanged(nameof(IsSidePanelDebugInfoVisible));
@@ -60,7 +76,7 @@ public partial class ViewportControlVM : BaseViewModel, IViewport
     private void OnScreenSizeChanged(SizeChangedEventArgs e)
     {
         Size newSize = e.NewSize.GetFloor();
-        ScreenSize = newSize;
+        _viewport.ScreenSize = newSize;
     }
 
     [RelayCommand]
@@ -68,12 +84,13 @@ public partial class ViewportControlVM : BaseViewModel, IViewport
     {
         Point oldMousePos = MousePos;
         Point newMousePos = e.GetPosition(e.Source as IInputElement).GetFloor();
-        Point newMousePosDelta = ((Point)(newMousePos - oldMousePos)).GetFloor();
+        Vector newMousePosDelta = (newMousePos - oldMousePos).GetFloor();
         MousePos = newMousePos;
-        MousePosDelta = MouseInitClickDrag && MouseClickHolding ? new Point(0, 0) : newMousePosDelta;
+        MousePosDelta = MouseInitClickDrag && MouseClickHolding ? new Vector(0, 0) : newMousePosDelta;
         if (MouseClickHolding)
         {
-            CameraPos -= ((Vector)MousePosDelta) / UnitScale;
+            Vector cameraPosDelta = MousePosDelta / _viewport.ZoomLevel;
+            _viewport.CameraPos += cameraPosDelta;
             MouseInitClickDrag = false;
         }
     }
@@ -83,13 +100,14 @@ public partial class ViewportControlVM : BaseViewModel, IViewport
     {
         double newZoomLevel;
         if (e.Delta > 0)
-            newZoomLevel = ZoomLevel * s_zoomRatio;
+            newZoomLevel = _viewport.ZoomLevel * s_zoomRatio;
         else
-            newZoomLevel = ZoomLevel / s_zoomRatio;
+            newZoomLevel = _viewport.ZoomLevel / s_zoomRatio;
         // limit zoom scrollability by 8 for zoom in, 2 for zoom out
-        ZoomLevel = Math.Clamp(newZoomLevel,
+        newZoomLevel = Math.Clamp(newZoomLevel,
                                1 / Math.Pow(s_zoomRatio, 2),
                                1 * Math.Pow(s_zoomRatio, 8));
+        _viewport.ZoomLevel = newZoomLevel;
     }
 
     [RelayCommand]
@@ -113,7 +131,7 @@ public partial class ViewportControlVM : BaseViewModel, IViewport
     private void OnMouseEnter()
     {
         MouseIsOutside = false;
-        RequestFocusToViewport?.Invoke();
+        _viewportView.Focus();
     }
 
     [RelayCommand]
@@ -127,12 +145,5 @@ public partial class ViewportControlVM : BaseViewModel, IViewport
     private void OnKey(KeyEventArgs e)
     {
         MessageBox.Show("Key Up!");
-    }
-
-    partial void OnZoomLevelChanged(double value)
-    {
-        OnPropertyChanged(nameof(PixelPerBlock));
-        OnPropertyChanged(nameof(PixelPerChunk));
-        OnPropertyChanged(nameof(PixelPerRegion));
     }
 }
