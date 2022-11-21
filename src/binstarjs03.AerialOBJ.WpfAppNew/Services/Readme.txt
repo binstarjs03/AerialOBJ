37d994d59ba0f12b8a7b80b4165667e52aab642d
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-11-21 17:02:02 +0700

    implement RegionCacheService

---
+++ src/binstarjs03.AerialOBJ.WpfAppNew/Services/Readme.txt
@@ -0,0 +1,4 @@
+﻿Service API are static, self-contained, and dependency-free API. 
+Therefore, any API in this folder MUST conforms these specifications.
+
+The only exception for service API is having dependency on SharedStateService.
---
+++ src/binstarjs03.AerialOBJ.WpfAppNew/Services/RegionCacheService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+using binstarjs03.AerialOBJ.Core.CoordinateSystem;
+using binstarjs03.AerialOBJ.Core.MinecraftWorld;
+
+namespace binstarjs03.AerialOBJ.WpfAppNew.Services;
+public static class RegionCacheService
+{
+    private static Dictionary<Coords2, Region> _regionCache = new();
+
+    public static bool HasRegion(Coords2 regionCoords)
+    {
+        return _regionCache.ContainsKey(regionCoords);
+    }
+
+    public static void Store(Region region)
+    {
+        if (HasRegion(region.RegionCoords))
+            return;
+        _regionCache.Add(region.RegionCoords, region);
+    }
+
+    public static void Delete(Coords2 regionCoords)
+    {
+        if (HasRegion(regionCoords))
+            _regionCache.Remove(regionCoords);
+    }
+
+    public static void Clear()
+    {
+        _regionCache.Clear();
+    }
+}
