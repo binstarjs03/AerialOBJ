62d1ae52d3f6ee049f9f4c0262e7fbbc4b65d63f
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-11-24 06:36:15 +0700

    implement ChunkViewport RegionModel load-unload

--- src/binstarjs03.AerialOBJ.Core/Visualization/TwoDimension/ChunkViewport2.cs
+++ src/binstarjs03.AerialOBJ.Core/Visualization/TwoDimension/ChunkViewport2.cs
@@ -1,15 +1,27 @@
-﻿using binstarjs03.AerialOBJ.Core.MinecraftWorld;
+﻿using System;
+using System.Collections.Generic;
+
+using binstarjs03.AerialOBJ.Core.MinecraftWorld;
 using binstarjs03.AerialOBJ.Core.Primitives;
 
 namespace binstarjs03.AerialOBJ.Core.Visualization.TwoDimension;
-
-public class ChunkViewport2<TRegionImage> : Viewport2 where TRegionImage : class, IRegionImage, new()
+//#pragma warning disable
+public abstract class ChunkViewport2<TRegionImage> : Viewport2 where TRegionImage : class, IRegionImage, new()
 {
     public event RegionImageEventHandler? RegionImageAdded;
     public event RegionImageEventHandler? RegionImageRemoved;
     public delegate void RegionImageEventHandler(TRegionImage regionImage);
 
+    private const int s_regionBufferSize = 20;
+    private const int s_chunkBufferSize = s_regionBufferSize * Region.TotalChunkCount;
+
+    private readonly Random _rng = new();
+
     private Point2ZRange<int> _visibleRegionRange;
+    private readonly Dictionary<Point2Z<int>, RegionModel<TRegionImage>> _loadedRegions = new(s_regionBufferSize);
+    private readonly List<Point2Z<int>> _pendingRegionList = new(s_regionBufferSize);
+    private Point2Z<int>? _workedRegion = null;
+
     private Point2ZRange<int> _visibleChunkRange;
 
     public Point2<int> ScreenCenter => (Point2<int>)(ScreenSize / 2);
@@ -27,9 +39,7 @@ public class ChunkViewport2<TRegionImage> : Viewport2 where TRegionImage : class
         if (RecalculateVisibleChunkRange())
         {
             if (RecalculateVisibleRegionRange())
-            {
-
-            }
+                LoadUnloadRegions();
         }
     }
 
@@ -83,5 +93,79 @@ public class ChunkViewport2<TRegionImage> : Viewport2 where TRegionImage : class
             return false;
         _visibleRegionRange = newVisibleRegionRange;
         return true;
+    }
+
+    protected virtual void LoadUnloadRegions()
+    {
+        // perform boundary range checking for regions outside display frame
+        // unload them if outside
+        foreach ((Point2Z<int> regionCoords, RegionModel<TRegionImage> regionModel) in _loadedRegions)
+            if (_visibleRegionRange.IsOutside(regionCoords))
+                UnloadRegionModel(regionModel);
+
+        // remove all pending region that is no longer visible
+        _pendingRegionList.RemoveAll(_visibleRegionRange.IsOutside);
+
+        // perform sweep checking from min to max range for visible regions
+        // add them to pending list if not loaded yet, not in the list, and is not worked yet
+        for (int x = _visibleRegionRange.XRange.Min; x <= _visibleRegionRange.XRange.Max; x++)
+            for (int z = _visibleRegionRange.ZRange.Min; z <= _visibleRegionRange.ZRange.Max; z++)
+            {
+                Point2Z<int> regionCoords = new(x, z);
+                if (!_pendingRegionList.Contains(regionCoords))
+                    _pendingRegionList.Add(regionCoords);
+            }
+        // base implementation goes here, may call LoadRegion(), must call base implementation()
+    }
+
+    protected void LoadRegion()
+    {
+        if (_pendingRegionList.Count == 0)
+            return;
+        int randomIndex = _rng.Next(0, _pendingRegionList.Count);
+        Point2Z<int> regionCoords = _pendingRegionList[randomIndex];
+        _pendingRegionList.RemoveAt(randomIndex);
+        _workedRegion = regionCoords;
+
+        Region? region = GetRegionFromCache(regionCoords);
+        if (region is null)
+            return;
+        RegionModel<TRegionImage> regionModel = new(region);
+        _loadedRegions.Add(regionCoords, regionModel);
+        RegionImageAdded?.Invoke(regionModel.RegionImage);
+    }
+
+    protected abstract Region? GetRegionFromCache(Point2Z<int> regionCoords);
+    /* Implementation of GetRegionFromCache for current implementation of WPF App
+     *
+     * Region region;
+     * // Try to fetch region from cache. If miss, load it from disk
+     * if (RegionCacheService.HasRegion(regionCoords))
+     *     region = RegionCacheService.Get(regionCoords);
+     * else
+     * {
+     *     Region? regionDisk = IOService.ReadRegionFile(regionCoords, out Exception? e);
+     *     // cancel region loading if we can't get the region at specified coords
+     *     // (corrupted, file not exist or not generated yet etc)
+     *     if (regionDisk is null)
+     *     {
+     *         if (e is not null)
+     *         {
+     *             // TODO display messagebox only once and never again
+     *             LogService.LogEmphasis($"Region {regionCoords} was skipped.", LogService.Emphasis.Error);
+     *             LogService.Log($"Cause of exception: {e.GetType()}");
+     *             LogService.Log($"Exception details: {e}", useSeparator: true);
+     *         }
+     *         return;
+     *     }
+     *     region = regionDisk;
+     *     RegionCacheService.Store(region);
+     * }
+     */
+
+    private void UnloadRegionModel(RegionModel<TRegionImage> regionModel)
+    {
+        _loadedRegions.Remove(regionModel.RegionCoords);
+        RegionImageRemoved?.Invoke(regionModel.RegionImage);
     }
 }
---
+++ src/binstarjs03.AerialOBJ.Core/Visualization/TwoDimension/RegionModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using binstarjs03.AerialOBJ.Core.MinecraftWorld;
+using binstarjs03.AerialOBJ.Core.Primitives;
+
+namespace binstarjs03.AerialOBJ.Core.Visualization.TwoDimension;
+
+public class RegionModel<TRegionImage> where TRegionImage : class, IRegionImage, new()
+{
+    public Region Region { get; }
+    public HashSet<Point2Z<int>> GeneratedChunks { get; }
+    public Point2Z<int> RegionCoords { get; }
+    public TRegionImage RegionImage { get; }
+
+    public RegionModel(Region region)
+    {
+        Region = region;
+        GeneratedChunks = region.GetGeneratedChunksAsCoordsRelSet();
+        RegionCoords = region.RegionCoords;
+        RegionImage = new TRegionImage();
+    }
+}
