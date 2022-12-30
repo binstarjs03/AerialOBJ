eccac6ea480d16a83ab673641c941f34a5f43822
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-12-30 14:10:19 +0700

    Initial implementation of DefinitionManagerService

---
+++ src/binstarjs03.AerialOBJ.WpfAppNew2/Services/DefinitionManagerService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using binstarjs03.AerialOBJ.Core.Definitions;
+
+namespace binstarjs03.AerialOBJ.WpfAppNew2.Services;
+public class DefinitionManagerService
+{
+    private ViewportDefinition _currentViewportDefinition;
+
+    public ViewportDefinition CurrentViewportDefinition => _currentViewportDefinition;
+    public List<ViewportDefinition> ViewportDefinitions { get; set; } = new();
+
+    public DefinitionManagerService()
+    {
+        _currentViewportDefinition = ViewportDefinition.GetDefaultDefinition();
+    }
+}
