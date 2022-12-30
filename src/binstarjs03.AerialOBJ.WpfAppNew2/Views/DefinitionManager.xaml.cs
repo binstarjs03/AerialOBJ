db66ed7cb1357ca2d8b39ecf5acaddc1a844a77c
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-12-30 14:20:25 +0700

    Initial implementation of Definition Manager View

---
+++ src/binstarjs03.AerialOBJ.WpfAppNew2/Views/DefinitionManager.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace binstarjs03.AerialOBJ.WpfAppNew2.Views;
+public partial class DefinitionManager : Window
+{
+    public DefinitionManager()
+    {
+        InitializeComponent();
+    }
+}
