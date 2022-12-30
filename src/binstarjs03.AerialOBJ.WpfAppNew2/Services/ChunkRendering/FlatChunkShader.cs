675262b869f08589ff27a8f9eba677554ba9de44
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2022-12-30 18:00:19 +0700

    Integrate Block Definition with Chunk Shader

--- src/binstarjs03.AerialOBJ.WpfAppNew2/Services/ChunkRendering/FlatChunkShader.cs
+++ src/binstarjs03.AerialOBJ.WpfAppNew2/Services/ChunkRendering/FlatChunkShader.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Threading;
 
+using binstarjs03.AerialOBJ.Core.Definitions;
 using binstarjs03.AerialOBJ.Core.MinecraftWorld;
 using binstarjs03.AerialOBJ.Core.Primitives;
 using binstarjs03.AerialOBJ.WpfAppNew2.Models;
@@ -19,18 +20,35 @@ public class FlatChunkShader : IChunkShader
 
     public void RenderChunk(RegionModel regionModel, Chunk chunk)
     {
-        ChunkHighestBlockInfo highestBlockInfo = new();
-        chunk.GetHighestBlock(highestBlockInfo);
+        // TODO reuse instance instead of creating new
+        ChunkHighestBlockInfo highestBlock = new();
+        chunk.GetHighestBlock(highestBlock);
+
         for (int x = 0; x < Section.BlockCount; x++)
             for (int z = 0; z < Section.BlockCount; z++)
             {
-                Color color = ...; // find color in block definition
-                Point2<int> pixelPos = new(chunk.ChunkCoordsRel.X * Section.BlockCount + x,
-                                           chunk.ChunkCoordsRel.X * Section.BlockCount + z);
-                regionModel.RegionImage[pixelPos.X, pixelPos.Y] = color;
+                Point2Z<int> blockCoordsRel = new(x, z);
+                Point2<int> pixelCoords = GetPixelCoords(chunk, blockCoordsRel);
+                Color color = GetBlockColor(_definitionManager.DefaultViewportDefinition, highestBlock, blockCoordsRel);
+                regionModel.RegionImage[pixelCoords.X, pixelCoords.Y] = color;
             }
         App.Current.Dispatcher.InvokeAsync(regionModel.RegionImage.Redraw, DispatcherPriority.Background);
     }
 
+    private static Color GetBlockColor(ViewportDefinition definition, ChunkHighestBlockInfo highestBlock, Point2Z<int> blockCoordsRel)
+    {
+        // try get color from block definition
+        // else return missing block color
+        string blockName = highestBlock.Names[blockCoordsRel.X, blockCoordsRel.Z];
+        if (definition.BlockDefinitions.TryGetValue(blockName, out ViewportBlockDefinition? bd))
+            return bd.Color;
+        else
+            return definition.MissingBlockDefinition.Color;
+    }
 
+    private static Point2<int> GetPixelCoords(Chunk chunk, Point2Z<int> blockCoordsRel)
+    {
+        return new Point2<int>(chunk.ChunkCoordsRel.X * Section.BlockCount + blockCoordsRel.X,
+                               chunk.ChunkCoordsRel.X * Section.BlockCount + blockCoordsRel.Z);
+    }
 }
