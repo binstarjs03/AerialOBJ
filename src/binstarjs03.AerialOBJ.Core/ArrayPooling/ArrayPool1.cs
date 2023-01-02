2d168578b4260ece6883a3cdb9139431505ca5c8
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-02 16:36:38 +0700

    Implement ArrayPooling

---
+++ src/binstarjs03.AerialOBJ.Core/ArrayPooling/ArrayPool1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace binstarjs03.AerialOBJ.Core.ArrayPooling;
+public class ArrayPool1<T>
+{
+    private readonly int _length;
+    private readonly Queue<T[]> _pool = new();
+
+    public ArrayPool1(int length)
+    {
+        _length = length;
+    }
+
+    public T[] Rent()
+    {
+        lock (_pool)
+            if (_pool.TryDequeue(out T[]? result))
+                return result;
+        return new T[_length];
+    }
+
+    public void Return(T[] array)
+    {
+        if (array.Length != _length)
+            throw new ArgumentException($"Returned array length is not equal to {_length}", nameof(array));
+        lock (_pool)
+            _pool.Enqueue(array);
+    }
+}
---
+++ src/binstarjs03.AerialOBJ.Core/ArrayPooling/ArrayPool3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace binstarjs03.AerialOBJ.Core.ArrayPooling;
+public class ArrayPool3<T>
+{
+    private readonly int _length1;
+    private readonly int _length2;
+    private readonly int _length3;
+    private readonly Queue<T[,,]> _pool = new();
+
+    public ArrayPool3(int length1, int length2, int length3)
+    {
+        _length1 = length1;
+        _length2 = length2;
+        _length3 = length3;
+    }
+
+    public T[,,] Rent()
+    {
+        lock (_pool)
+            if (_pool.TryDequeue(out T[,,]? result))
+                return result;
+        return new T[_length1, _length2, _length3];
+    }
+
+    public void Return(T[,,] array)
+    {
+        if (array.GetLength(0) != _length1 || array.GetLength(1) != _length2 || array.GetLength(2) != _length3)
+            throw new ArgumentException($"Returned array length is not equal to [{_length1}, {_length2}, {_length3}]", nameof(array));
+        lock (_pool)
+            _pool.Enqueue(array);
+    }
+}
