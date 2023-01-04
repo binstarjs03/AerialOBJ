9ac503a97a1fdbafa7a7ef6593b691186945c984
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-04 21:13:57 +0700

    Implement ViewportDefinitionLoaderService

---
+++ src/binstarjs03.AerialOBJ.WpfApp/Services/IViewportDefinitionLoaderService.cs
@@ -0,0 +1,7 @@
+﻿using binstarjs03.AerialOBJ.Core.Definitions;
+
+namespace binstarjs03.AerialOBJ.WpfApp.Services;
+public interface IViewportDefinitionLoaderService
+{
+    ViewportDefinition ImportDefinitionFile(string path);
+}
---
+++ src/binstarjs03.AerialOBJ.WpfApp/Services/ViewportDefinitionLoaderService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+using binstarjs03.AerialOBJ.Core.Definitions;
+
+namespace binstarjs03.AerialOBJ.WpfApp.Services;
+public class ViewportDefinitionLoaderService : IViewportDefinitionLoaderService
+{
+    private readonly GlobalState _globalState;
+
+    public ViewportDefinitionLoaderService(GlobalState globalState)
+    {
+        _globalState = globalState;
+    }
+
+    public ViewportDefinition ImportDefinitionFile(string path)
+    {
+        string input = File.ReadAllText(path);
+        ViewportDefinition definition = ParseJson(input);
+        CopyToDefinitionFolder(path);
+        return definition;
+    }
+
+    private static ViewportDefinition ParseJson(string input)
+    {
+        ViewportDefinition? definition = JsonSerializer.Deserialize<ViewportDefinition>(input);
+        if (definition is null)
+            throw new NullReferenceException("Resulting deserialized definition is null");
+        return definition;
+    }
+
+    private void CopyToDefinitionFolder(string originalFilePath)
+    {
+        FileInfo fileInfo = new(originalFilePath);
+        Directory.CreateDirectory(_globalState.DefinitionsPath);
+        fileInfo.CopyTo(_globalState.DefinitionsPath);
+    }
+}
