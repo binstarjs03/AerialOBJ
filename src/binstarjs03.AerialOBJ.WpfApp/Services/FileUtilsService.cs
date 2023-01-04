dbeedd5f72abe5796da5fa6adfe09b45b5b098b6
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-04 09:38:40 +0700

    Implement FileUtilsService

---
+++ src/binstarjs03.AerialOBJ.WpfApp/Services/FileUtilsService.cs
@@ -0,0 +1,8 @@
+﻿using System.IO;
+
+namespace binstarjs03.AerialOBJ.WpfApp.Services;
+public class FileUtilsService : IFileUtilsService
+{
+    public bool Exist(string path) => File.Exists(path);
+    public string ReadAllText(string path) => File.ReadAllText(path);
+}
---
+++ src/binstarjs03.AerialOBJ.WpfApp/Services/IFileUtilsService.cs
@@ -0,0 +1,6 @@
+﻿namespace binstarjs03.AerialOBJ.WpfApp.Services;
+public interface IFileUtilsService
+{
+    bool Exist(string path);
+    string ReadAllText(string path);
+}
