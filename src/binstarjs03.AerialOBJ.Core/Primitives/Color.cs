173f4a35fc9ed7f5c30d5efec4d003cfc829fb87
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-04 19:07:30 +0700

    Remove unnecessary IColor interface

--- src/binstarjs03.AerialOBJ.Core/Primitives/Color.cs
+++ src/binstarjs03.AerialOBJ.Core/Primitives/Color.cs
@@ -1,10 +1,23 @@
-﻿namespace binstarjs03.AerialOBJ.Core.Primitives;
-public struct Color : IColor
+﻿using System.Globalization;
+
+namespace binstarjs03.AerialOBJ.Core.Primitives;
+public struct Color
 {
     public required byte Red { get; set; }
     public required byte Green { get; set; }
     public required byte Blue { get; set; }
     public required byte Alpha { get; set; }
+
+    public static Color Parse(string hexColor, byte alpha)
+    {
+        return new Color()
+        {
+            Red = byte.Parse(hexColor[1..3], NumberStyles.HexNumber),
+            Green = byte.Parse(hexColor[3..5], NumberStyles.HexNumber),
+            Blue = byte.Parse(hexColor[5..7], NumberStyles.HexNumber),
+            Alpha = alpha
+        };
+    }
 
     public override string ToString()
     {
--- src/binstarjs03.AerialOBJ.Core/Primitives/IColor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace binstarjs03.AerialOBJ.Core.Primitives;
-
-public interface IColor
-{
-    byte Red { get; }
-    byte Green { get; }
-    byte Blue { get; }
-    byte Alpha { get; }
-}
