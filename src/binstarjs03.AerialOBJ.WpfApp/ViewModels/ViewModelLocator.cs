8058b6ece9e481f10752d01ac37519b87c8b40e2
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-04 12:57:42 +0700

    Rename AppResourceBootstrapper to ViewModelLocator

---
+++ src/binstarjs03.AerialOBJ.WpfApp/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace binstarjs03.AerialOBJ.WpfApp.ViewModels;
+public class ViewModelLocator
+{
+    public ViewModelLocator()
+    {
+        AbstractViewModel = App.Current?.ServiceProvider.GetRequiredService<AbstractViewModel>()!;
+    }
+
+    public AbstractViewModel AbstractViewModel { get; }
+}
