d4c2c7defe63aad8736e7d2906434ed886c366c7
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-14 20:50:34 +0700

    Definition format update
    format version incremented when there is a change to the format and when published to public release

---
+++ src/binstarjs03.AerialOBJ.Core/Definitions/JSONConverters/JSONViewportBlockDefinitionConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using binstarjs03.AerialOBJ.Core.Primitives;
+
+namespace binstarjs03.AerialOBJ.Core.Definitions.JSONConverters;
+public class JSONViewportBlockDefinitionConverter : JsonConverter<ViewportBlockDefinition>
+{
+    public override ViewportBlockDefinition? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        try
+        {
+            // advance to property name "Color"
+            reader.Read();
+
+            // advance to "Color" string value
+            reader.Read();
+            string colorStr = reader.GetString()!;
+
+            // advance to property name "Alpha"
+            reader.Read();
+
+            // advance to "Alpha" number value
+            reader.Read();
+            byte alpha = reader.GetByte();
+
+            // advance to property name "DisplayName"
+            reader.Read();
+
+            // advance to "DisplayName" string value
+            reader.Read();
+            string displayName = reader.GetString()!;
+
+            // advance to end object
+            reader.Read();
+
+            return new ViewportBlockDefinition()
+            {
+                Color = new Color
+                {
+                    Alpha = alpha,
+                    Red = byte.Parse(colorStr[1..3], NumberStyles.HexNumber),
+                    Green = byte.Parse(colorStr[3..5], NumberStyles.HexNumber),
+                    Blue = byte.Parse(colorStr[5..7], NumberStyles.HexNumber),
+                },
+                DisplayName = displayName,
+            };
+        }
+        catch (Exception e)
+        {
+            throw new JsonException("Invalid JSON format", e);
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, ViewportBlockDefinition value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/binstarjs03.AerialOBJ.Core/Definitions/ViewportBlockDefinition.cs
+++ src/binstarjs03.AerialOBJ.Core/Definitions/ViewportBlockDefinition.cs
@@ -5,9 +5,9 @@ using binstarjs03.AerialOBJ.Core.Primitives;
 
 namespace binstarjs03.AerialOBJ.Core.Definitions;
 
+[JsonConverter(typeof(JSONViewportBlockDefinitionConverter))]
 public class ViewportBlockDefinition
 {
-    [JsonConverter(typeof(JSONColorConverter))]
     public required Color Color { get; set; }
     public required string DisplayName { get; set; }
 
