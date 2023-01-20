b5d9abd906c3b8a618f190c540d8791229271d2d
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-20 08:52:20 +0700

    Folder organizing chunk pattern

---
+++ src/binstarjs03.AerialOBJ.WpfApp/Services/ChunkRegionManaging/Patterns/SplitChunkLoadingPattern.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace binstarjs03.AerialOBJ.WpfApp.Services.ChunkRegionManaging;
+
+public class SplitChunkLoadingPattern : IChunkLoadingPattern
+{
+    public int GetPendingChunkIndex(int pendingChunkCount)
+    {
+        return Math.Min(pendingChunkCount / 2, pendingChunkCount - 1);
+    }
+}
