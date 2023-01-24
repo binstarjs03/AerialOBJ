2c3b3dba555deb433a86589dd2eb2287089fc860
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-24 17:01:31 +0700

    use abstract dispatcher

--- src/binstarjs03.AerialOBJ.WpfApp/Factories/RegionImageFactory.cs
+++ src/binstarjs03.AerialOBJ.WpfApp/Factories/RegionImageFactory.cs
@@ -1,18 +1,25 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Threading;
 
 using binstarjs03.AerialOBJ.Core.Primitives;
 using binstarjs03.AerialOBJ.WpfApp.Components;
+using binstarjs03.AerialOBJ.WpfApp.Services.Dispatcher;
 
 namespace binstarjs03.AerialOBJ.WpfApp.Factories;
 public class RegionImageFactory : IRegionImageFactory
 {
+    private readonly IDispatcher _dispatcher;
+
+    public RegionImageFactory(IDispatcher dispatcher)
+    {
+        _dispatcher = dispatcher;
+    }
+
     public IRegionImage Create(PointZ<int> regionCoords, CancellationToken cancellationToken)
     {
-        if (App.Current.CheckAccess())
+        if (_dispatcher.CheckAccess())
             return new RegionImage(regionCoords);
-        RegionImage? result = App.Current.Dispatcher.Invoke(() => new RegionImage(regionCoords), DispatcherPriority.Background, cancellationToken);
+        RegionImage? result = _dispatcher.Invoke(() => new RegionImage(regionCoords), DispatcherPriority.Background, cancellationToken);
         if (result is null)
             throw new TaskCanceledException();
         return result;
