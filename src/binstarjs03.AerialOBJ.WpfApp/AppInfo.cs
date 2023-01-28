c362386d7ca06762c05dde37d6010a579b4fb379
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-01-28 18:09:17 +0700

    Implement AppInfo

---
+++ src/binstarjs03.AerialOBJ.WpfApp/AppInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace binstarjs03.AerialOBJ.WpfApp;
+public class AppInfo
+{
+    public required string AppName { get; set; }
+    public required string Version { get; set; }
+    public required DateTime LaunchTime { get; set; }
+    public required ConstantPath Path { get; set; }
+    public required string[]? Arguments { get; set; }
+    public bool IsDebugEnabled => Arguments is not null && Arguments.All(arg=> arg.ToLower() == "debug");
+}
