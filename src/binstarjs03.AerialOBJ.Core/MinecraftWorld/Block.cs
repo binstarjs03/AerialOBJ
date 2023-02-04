5edb0a5fd5cffcad4b5e750451a92a4e0b1ae16f
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-04 13:23:08 +0700

    MinecraftWorld refactor hard-coded literal with constant

--- src/binstarjs03.AerialOBJ.Core/MinecraftWorld/Block.cs
+++ src/binstarjs03.AerialOBJ.Core/MinecraftWorld/Block.cs
@@ -3,22 +3,26 @@
 namespace binstarjs03.AerialOBJ.Core.MinecraftWorld;
 public struct Block
 {
+    public const string AirBlockName = "minecraft:air";
+    public const string CaveAirBlockName = "minecraft:cave_air";
+
     private static readonly Block s_air = new()
     {
-        Name = "minecraft:air",
-        Coords = new Point3<int>(0, 0, 0)
+        Name = AirBlockName,
+        Coords = Point3<int>.Zero,
     };
 
     public Block() { }
 
     public static ref readonly Block Air => ref s_air;
-    public required string Name { get; set; } = "minecraft:air";
+
+    public required string Name { get; set; } = AirBlockName;
     public required Point3<int> Coords { get; set; } = Point3<int>.Zero;
 
     // TODO maybe we should use definitions and check if Name is in list
     // of air blocks instead of hardcoding it to some string literal
-    public bool IsAir => Name == "minecraft:air"
-                      || Name == "minecraft:cave_air";
+    public bool IsAir => Name == AirBlockName
+                      || Name == CaveAirBlockName;
 
     public override string ToString()
     {
