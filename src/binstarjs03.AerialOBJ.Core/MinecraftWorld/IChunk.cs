b14a9a9ec733cb918095ccb4e8aeec57d904d5cd
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-05 07:56:56 +0700

    IChunk xml documentation

--- src/binstarjs03.AerialOBJ.Core/MinecraftWorld/IChunk.cs
+++ src/binstarjs03.AerialOBJ.Core/MinecraftWorld/IChunk.cs
@@ -1,19 +1,53 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 using binstarjs03.AerialOBJ.Core.Definitions;
 using binstarjs03.AerialOBJ.Core.Primitives;
 
 namespace binstarjs03.AerialOBJ.Core.MinecraftWorld;
+
+/// <summary>
+/// Represent Chunk Data
+/// </summary>
 public interface IChunk : IDisposable
 {
+    /// <summary>
+    /// Block count in single dimension in section
+    /// </summary>
     const int BlockCount = 16;
+
+    /// <summary>
+    /// Block index range for indexing. Use this to write cleaner for loop
+    /// instead of having to write "BlockCount - 1"
+    /// </summary>
     const int BlockRange = BlockCount - 1;
 
+    /// <summary>
+    /// Returns the minimum supported DataVersion for this chunk
+    /// </summary>
     int DataVersion { get; }
+
+    /// <summary>
+    /// Returns the minimum supported Minecraft version for this chunk
+    /// </summary>
     string MinecraftVersion { get; } // example: "1.18.2"
+
+    /// <summary>
+    /// Returns absolute position of this chunk
+    /// </summary>
     PointZ<int> CoordsAbs { get; }
+
+    /// <summary>
+    /// Returns relative position of this chunk, relative to region
+    /// </summary>
     PointZ<int> CoordsRel { get; }
 
+    /// <summary>
+    /// Get highest block of this chunk, starting from <paramref name="heightLimit"/>
+    /// and scans it to the lowest block of lowest section of this chunk. 
+    /// It is recommended for implementors to analyze the sections first to find 
+    /// the highest starting non-excluded section and store it in private field,
+    /// if this is the first time this method called
+    /// </summary>
     void GetHighestBlockSlim(ViewportDefinition vd, BlockSlim[,] buffer, int heightLimit);
 }
