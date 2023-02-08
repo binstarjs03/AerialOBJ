a0a5042d92015749e64c8236d55a4999a9df1270
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-08 08:52:12 +0700

    Implement PointZ Extension

---
+++ src/binstarjs03.AerialOBJ.Core/Primitives/PointZExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace binstarjs03.AerialOBJ.Core.Primitives;
+public static class PointZExtension
+{
+    public static PointZ<TNumber> MoveNorth<TNumber>(this PointZ<TNumber> point, TNumber? distance = null) where TNumber : struct, INumber<TNumber>
+    {
+        distance ??= TNumber.One;
+        point.Z -= distance.Value;
+        return point;
+    }
+
+    public static PointZ<TNumber> MoveSouth<TNumber>(this PointZ<TNumber> point, TNumber? distance = null) where TNumber : struct, INumber<TNumber>
+    {
+        distance ??= TNumber.One;
+        point.Z += distance.Value;
+        return point;
+    }
+
+    public static PointZ<TNumber> MoveWest<TNumber>(this PointZ<TNumber> point, TNumber? distance = null) where TNumber : struct, INumber<TNumber>
+    {
+        distance ??= TNumber.One;
+        point.X -= distance.Value;
+        return point;
+    }
+
+    public static PointZ<TNumber> MoveEast<TNumber>(this PointZ<TNumber> point, TNumber? distance = null) where TNumber : struct, INumber<TNumber>
+    {
+        distance ??= TNumber.One;
+        point.X += distance.Value;
+        return point;
+    }
+}
