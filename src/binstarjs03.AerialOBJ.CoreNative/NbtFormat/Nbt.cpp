b22b9b4b0d6fe4338a9e20365fd24031671e382e
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-14 18:09:09 +0700

    Native Nbt constructors

--- src/binstarjs03.AerialOBJ.CoreNative/NbtFormat/Nbt.cpp
+++ src/binstarjs03.AerialOBJ.CoreNative/NbtFormat/Nbt.cpp
@@ -1,52 +1,150 @@
 #include "Nbt.h"
 #include "NbtType.h"
 
+// Nbt Abstract
+
 NbtType Nbt::GetType() {
     return NbtType::NbtEnd;
+}
+
+
+
+// Nbt Byte
+
+NbtByte::NbtByte() {
+    this->Value = 0;
 }
 
 NbtType NbtByte::GetType() {
     return NbtType::NbtByte;
 }
 
+
+
+// Nbt Short
+
+NbtShort::NbtShort() {
+    this->Value = 0;
+}
+
 NbtType NbtShort::GetType() {
     return NbtType::NbtShort;
+}
+
+
+
+// Nbt Int
+
+NbtInt::NbtInt() {
+    this->Value = 0;
 }
 
 NbtType NbtInt::GetType() {
     return NbtType::NbtInt;
 }
 
+
+
+// Nbt Long
+
+NbtLong::NbtLong() {
+    this->Value = 0;
+}
+
 NbtType NbtLong::GetType() {
     return NbtType::NbtLong;
+}
+
+
+
+// Nbt Float
+
+NbtFloat::NbtFloat() {
+    this->Value = 0;
 }
 
 NbtType NbtFloat::GetType() {
     return NbtType::NbtFloat;
 }
 
+
+
+// Nbt Double
+
+NbtDouble::NbtDouble() {
+    this->Value = 0;
+}
+
 NbtType NbtDouble::GetType() {
     return NbtType::NbtDouble;
+}
+
+
+
+// Nbt String
+
+NbtString::NbtString() {
+    this->Value = "";
 }
 
 NbtType NbtString::GetType() {
     return NbtType::NbtString;
 }
 
+
+
+// Nbt Byte Array
+
+NbtByteArray::NbtByteArray() {
+    this->Values = std::vector<int8_t>();
+}
+
 NbtType NbtByteArray::GetType() {
     return NbtType::NbtByteArray;
 }
 
-NbtType NbtLongArray::GetType() {
-    return NbtType::NbtLongArray;
+
+
+// Nbt Int Array
+
+NbtIntArray::NbtIntArray() {
+    this->Values = std::vector<int32_t>();
 }
 
 NbtType NbtIntArray::GetType() {
     return NbtType::NbtIntArray;
 }
 
+
+
+// Nbt Long Array
+
+NbtLongArray::NbtLongArray() {
+    this->Values = std::vector<int64_t>();
+}
+
+NbtType NbtLongArray::GetType() {
+    return NbtType::NbtLongArray;
+}
+
+
+
+// Nbt Compound
+
+NbtCompound::NbtCompound() {
+    this->Nbts = std::map<std::string, Nbt>();
+}
+
 NbtType NbtCompound::GetType() {
     return NbtType::NbtCompound;
+}
+
+
+
+// Nbt List
+
+NbtList::NbtList() {
+    this->Nbts = std::vector<Nbt>();
 }
 
 NbtType NbtList::GetType() {
--- src/binstarjs03.AerialOBJ.CoreNative/NbtFormat/Nbt.h
+++ src/binstarjs03.AerialOBJ.CoreNative/NbtFormat/Nbt.h
@@ -9,76 +9,89 @@ class Nbt
 public:
     std::string Name;
     virtual NbtType GetType();
+protected:
 };
 
 class NbtByte : public Nbt {
 public:
+    NbtByte();
     std::int8_t Value;
     NbtType GetType() override;
 };
 
 class NbtShort : public Nbt {
 public:
+    NbtShort();
     std::int16_t Value;
     NbtType GetType() override;
 };
 
 class NbtInt : public Nbt {
 public:
+    NbtInt();
     std::int32_t Value;
     NbtType GetType() override;
 };
 
 class NbtLong : public Nbt {
 public:
+    NbtLong();
     std::int64_t Value;
     NbtType GetType() override;
 };
 
 class NbtFloat : public Nbt {
 public:
+    NbtFloat();
     float Value;
     NbtType GetType() override;
 };
 
 class NbtDouble : public Nbt {
 public:
+    NbtDouble();
     double Value;
     NbtType GetType() override;
 };
 
 class NbtString : public Nbt {
 public:
+    NbtString();
     std::string Value;
     NbtType GetType() override;
 };
 
 class NbtByteArray : public Nbt {
 public:
+    NbtByteArray();
     std::vector<int8_t> Values;
     NbtType GetType() override;
 };
 
 class NbtIntArray : public Nbt {
 public:
+    NbtIntArray();
     std::vector<int32_t> Values;
     NbtType GetType() override;
 };
 
 class NbtLongArray : public Nbt {
 public:
+    NbtLongArray();
     std::vector<int64_t> Values;
     NbtType GetType() override;
 };
 
 class NbtCompound : public Nbt {
 public:
+    NbtCompound();
     std::map<std::string, Nbt> Nbts;
     NbtType GetType() override;
 };
 
 class NbtList : public Nbt {
 public:
+    NbtList();
     std::vector<Nbt> Nbts;
     NbtType GetType() override;
 };
