d7949f0ef598bdfdadd16793376478c58fe10561
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-15 10:53:31 +0700

    Implement IntBox

---
+++ src/binstarjs03.AerialOBJ.WpfApp/Components/IntBox.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace binstarjs03.AerialOBJ.WpfApp.Components;
+public partial class IntBox : UserControl
+{
+    public IntBox()
+    {
+        InitializeComponent();
+    }
+
+    public int IntValue
+    {
+        get => (int)GetValue(IntValueProperty);
+        set => SetValue(IntValueProperty, value);
+    }
+
+    public static readonly DependencyProperty IntValueProperty =
+        DependencyProperty.Register(nameof(IntValue), typeof(int), typeof(IntBox), new PropertyMetadata(0));
+
+    private void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+            UpdateValueBinding();
+    }
+
+    private void OnLostFocus(object sender, RoutedEventArgs e)
+    {
+        UpdateValueBinding();
+    }
+
+    private void UpdateValueBinding()
+    {
+        BindingExpression expr = UnderlyingTextBox.GetBindingExpression(TextBox.TextProperty);
+        expr.UpdateSource();
+    }
+}
