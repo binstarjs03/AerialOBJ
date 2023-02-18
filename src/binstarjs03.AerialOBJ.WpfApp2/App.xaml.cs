6f696f85d9abf4a139310dffbbdd39b63c37d25c
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-18 12:05:39 +0700

    service config

--- src/binstarjs03.AerialOBJ.WpfApp2/App.xaml.cs
+++ src/binstarjs03.AerialOBJ.WpfApp2/App.xaml.cs
@@ -1,6 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace binstarjs03.AerialOBJ.WpfApp;
 public partial class App : Application
 {
+    public static new App Current => (Application.Current as App)!;
+
+#pragma warning disable CS8618 // nullable warning
+    public IServiceProvider ServiceProvider { get; private set; }
+#pragma warning restore CS8618
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        ShutdownMode = ShutdownMode.OnMainWindowClose;
+        ServiceProvider = ServiceConfiguration.Configure();
+    }
 }
---
+++ src/binstarjs03.AerialOBJ.WpfApp2/ServiceConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace binstarjs03.AerialOBJ.WpfApp;
+
+public static class ServiceConfiguration
+{
+    public static IServiceProvider Configure()
+    {
+        IServiceCollection services = new ServiceCollection();
+        return services.BuildServiceProvider();
+    }
+}
