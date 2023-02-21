26713b83bd4aecaf0f5eb2d5ccb75df25b8c22f7
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-21 14:49:36 +0700

    Implement IMouse

---
+++ src/binstarjs03.AerialOBJ.MvvmAppCore/Services/Input/IMouse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using binstarjs03.AerialOBJ.Core.Primitives;
+
+using CommunityToolkit.Mvvm.Input;
+
+namespace binstarjs03.AerialOBJ.MvvmAppCore.Services.Input;
+public interface IMouse
+{
+    PointY<int> MousePos { get; set; }
+    PointY<int> MouseDelta { get; }
+    bool IsMouseInside { get; }
+    bool IsMouseDown { get; }
+    bool IsMouseLeft { get; }
+    bool IsMouseRight { get; }
+    int ScrollDelta { get; }
+
+    IRelayCommand<object?> MouseMoveCommand { get; }
+    IRelayCommand<object?> MouseWheelCommand { get; }
+    IRelayCommand<object?> MouseUpCommand { get; }
+    IRelayCommand<object?> MouseDownCommand { get; }
+    IRelayCommand<object?> MouseEnterCommand { get; }
+    IRelayCommand<object?> MouseLeaveCommand { get; }
+
+    void RegisterHandler(Action<IMouse> handler, MouseHandlingCondition condition, MouseWhen when);
+}
---
+++ src/binstarjs03.AerialOBJ.MvvmAppCore/Services/Input/MouseWhen.cs
@@ -0,0 +1,11 @@
+﻿namespace binstarjs03.AerialOBJ.MvvmAppCore.Services.Input;
+
+public enum MouseWhen
+{
+    MouseMove,
+    MouseLeave,
+    MouseEnter,
+    MouseDown,
+    MouseUp,
+    MouseWheel
+}
