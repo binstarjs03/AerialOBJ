e382aac09db25841008ccee5b7c7f8e1ef601456
Bintang Jakasurya <93080026+binstarjs03@users.noreply.github.com>
2023-02-22 13:29:31 +0700

    Use ViewportInfo attached property

---
+++ src/binstarjs03.AerialOBJ.WpfApp2/Views/ViewportGridDrawer.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace binstarjs03.AerialOBJ.WpfApp.Views;
+public partial class ViewportGridDrawer : UserControl
+{
+    public ViewportGridDrawer()
+    {
+        InitializeComponent();
+    }
+
+
+
+    public bool IsRegionGridVisible
+    {
+        get => (bool)GetValue(IsRegionGridVisibleProperty);
+        set => SetValue(IsRegionGridVisibleProperty, value);
+    }
+
+    public static readonly DependencyProperty IsRegionGridVisibleProperty =
+        DependencyProperty.Register(nameof(IsRegionGridVisible),
+                                    typeof(bool),
+                                    typeof(ViewportGridDrawer),
+                                    new PropertyMetadata(false));
+
+    public bool IsChunkGridVisible
+    {
+        get => (bool)GetValue(IsChunkGridVisibleProperty);
+        set => SetValue(IsChunkGridVisibleProperty, value);
+    }
+
+    public static readonly DependencyProperty IsChunkGridVisibleProperty =
+        DependencyProperty.Register(nameof(IsChunkGridVisible),
+                                    typeof(bool),
+                                    typeof(ViewportGridDrawer),
+                                    new PropertyMetadata(false));
+}
--- src/binstarjs03.AerialOBJ.WpfApp2/Views/ViewportRegionImagePresenter.xaml.cs
+++ src/binstarjs03.AerialOBJ.WpfApp2/Views/ViewportRegionImagePresenter.xaml.cs
@@ -2,7 +2,6 @@
 using System.Windows;
 using System.Windows.Controls;
 
-using binstarjs03.AerialOBJ.Core.Primitives;
 using binstarjs03.AerialOBJ.MvvmAppCore.Models;
 
 namespace binstarjs03.AerialOBJ.WpfApp.Views;
@@ -23,40 +22,4 @@ public partial class ViewportRegionImagePresenter : UserControl
                                     typeof(ObservableCollection<RegionDataImageModel>),
                                     typeof(ViewportRegionImagePresenter),
                                     new PropertyMetadata(null));
-
-    public PointZ<float> CameraPos
-    {
-        get => (PointZ<float>)GetValue(CameraPosProperty);
-        set => SetValue(CameraPosProperty, value);
-    }
-
-    public static readonly DependencyProperty CameraPosProperty =
-        DependencyProperty.Register(nameof(CameraPos),
-                                    typeof(PointZ<float>),
-                                    typeof(ViewportRegionImagePresenter),
-                                    new PropertyMetadata(PointZ<float>.Zero));
-
-    public Size<int> ScreenSize
-    {
-        get => (Size<int>)GetValue(ScreenSizeProperty);
-        set => SetValue(ScreenSizeProperty, value);
-    }
-
-    public static readonly DependencyProperty ScreenSizeProperty =
-        DependencyProperty.Register(nameof(ScreenSize),
-                                    typeof(Size<int>),
-                                    typeof(ViewportRegionImagePresenter),
-                                    new PropertyMetadata(new Size<int>(1, 1)));
-
-    public float ZoomMultiplier
-    {
-        get => (float)GetValue(ZoomMultiplierProperty);
-        set => SetValue(ZoomMultiplierProperty, value);
-    }
-
-    public static readonly DependencyProperty ZoomMultiplierProperty =
-        DependencyProperty.Register(nameof(ZoomMultiplier),
-                                    typeof(float),
-                                    typeof(ViewportRegionImagePresenter),
-                                    new PropertyMetadata(1f));
 }
